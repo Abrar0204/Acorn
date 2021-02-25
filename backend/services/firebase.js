1253d13e0970f0b9a47bf009f48d554241cecbbf
Abrar Hasan <abrarhasan0204@gmail.com>
2021-02-25 12:59:34 +0530

    Added env variables

--- backend/services/firebase.js
+++ backend/services/firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceKey/serviceKey.json");
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert({
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+		client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	}),
 });
 
 const db = admin.firestore();
