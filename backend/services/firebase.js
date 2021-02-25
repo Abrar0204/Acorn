92c86f9cbf541a27828cfa6704bc8c76af049ba3
Abrar Hasan <abrarhasan0204@gmail.com>
2021-02-25 12:37:05 +0530

    Added serviceKey

--- backend/services/firebase.js
+++ backend/services/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceKey/acorn-3e774-firebase-adminsdk-gt1pk-557cfd5c1e.json");
+const serviceAccount = require("../serviceKey/serviceKey.json");
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
